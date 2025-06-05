924d57df7cfda3f19d4203b0e9253773a6041a5d
HamidHamidd <xamid.alii@gmail.com>
2025-06-05 18:17:13 +0300

    Add project files.

---
+++ DomashenLubimecX/Controllers/PetController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DomashenLubimecX.Data;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DomashenLubimecX.Models;
+
+namespace DomashenLubimecX.Controllers
+{
+    public class PetController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PetController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Info()
+        {
+            var petInfo = await _context.PetInfos.FirstOrDefaultAsync();
+            return View(petInfo);
+        }
+    }
+}
---
+++ DomashenLubimecX/Models/PetInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace DomashenLubimecX.Models
+{
+    public class PetInfo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
